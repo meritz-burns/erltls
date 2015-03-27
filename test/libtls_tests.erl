39024b5da4d41f8c5029201cc5d8d7479b905010
Rebecca Meritz <rebecca@meritz.com>
2015-03-27 17:55:49 +0100

    Improve protocol parse test

--- test/libtls_tests.erl
+++ test/libtls_tests.erl
@@ -4,12 +4,9 @@
 -define(setup(F), {setup, fun start/0, fun stop/1, F}).
 
 tls_config_test_() ->
-  ?setup(fun(_) ->
-             {ok, ConfigRefNo} = libtls:tls_config_new(),
-             {ok, ProtocolFlagsNo} = libtls:tls_config_parse_protocols("tlsv1.2"),
+  ?setup(fun(ConfigRefNo) ->
              [
-               ?_assertEqual(8, ProtocolFlagsNo)
-             , ?_assertEqual(ok, libtls:tls_config_set_ca_file(
+               ?_assertEqual(ok, libtls:tls_config_set_ca_file(
                                ConfigRefNo, "/fake/ca.crt"))
              , ?_assertEqual(ok, libtls:tls_config_set_ca_path(
                                ConfigRefNo, "/fake-path/"))
@@ -21,12 +18,34 @@ tls_config_test_() ->
              ]
           end).
 
+tls_protocol_test_() ->
+  ?setup(fun(_ConfigRefNo) ->
+             {ok, TLSV10} = libtls:tls_config_parse_protocols("tlsv1.0"),
+             {ok, TLSV11} = libtls:tls_config_parse_protocols("tlsv1.1"),
+             {ok, TLSV12} = libtls:tls_config_parse_protocols("tlsv1.2"),
+             {ok, TLSV1211} = libtls:tls_config_parse_protocols(
+                                "tlsv1.2, tlsv1.1"),
+             {ok, TLSV1210} = libtls:tls_config_parse_protocols(
+                                "tlsv1.2, tlsv1.0"),
+             [
+               ?_assertEqual(2, TLSV10)
+             , ?_assertEqual(4, TLSV11)
+             , ?_assertEqual(8, TLSV12)
+             , ?_assertEqual(8 bor 2, TLSV1210)
+             , ?_assertEqual(8 bor 4, TLSV1211)
+             ]
+          end).
+
+
 
 start() ->
   Pid = libtls:start(),
   timer:sleep(1),
   Pid = whereis(libtls),
-  ok = libtls:tls_init().
+  ok = libtls:tls_init(),
+  {ok, ConfigRefNo} = libtls:tls_config_new(),
+  ConfigRefNo.
 
-stop(_) ->
+stop(ConfigRefNo) ->
+  ok = libtls:tls_config_free(ConfigRefNo),
   libtls:stop().
