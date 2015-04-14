b28970323fa137da698b106dc2d190b05ce5c5d7
Mike Burns <mike@mike-burns.com>
2015-04-14 22:24:00 +0200

    Some sample C client cleanups
    It seems that the branch in which I was hacking on a C client got
    merged, and messily, so allow me to commit some improvements to that and
    intentionally merge this branch.
    
    Only some of the history is being re-written here because of the
    surprise merge and a desire not to futz with git too much.
    
    The improvements:
    
    - Only pass one cipher to tls_config_set_ciphers(3), for simplicity and
      readability.
    - Portable systems might not have a hardcoded path to the system cert
      files, so allow people to pass it manually via the command line.

--- test/client.c
+++ test/client.c
@@ -3,9 +3,13 @@
  *   gcc -g -std=c99 -Wall -Wextra -pedantic-errors -Werror \
  *     $(pkg-config libssl --cflags --libs) -ltls client.c -o client
  *
+ *   ./client
+ *
  * Debian:
  *   gcc -g -std=c99 -Wall -Wextra -pedantic-errors -Werror \
  *     $(pkg-config libtls libssl --cflags --libs) client.c -o client
+ *
+ *   ./client /etc/ssl/certs/ca-certificates.crt
  *
  */
 
@@ -33,13 +37,13 @@ static int	from_server(struct tls *, char *, size_t, size_t *);
  * Connect to mike-burns.com via HTTPS and print the home page to stdout.
  */
 int
-main()
+main(int argc, char *argv[])
 {
 	struct tls_config	*config;
 	struct tls		*ctx;
 	size_t			 outlen;
 	char			*buf;
-	const char		*ciphers = "ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:SRP-DSS-AES-256-CBC-SHA:SRP-RSA-AES-256-CBC-SHA:SRP-AES-256-CBC-SHA:DHE-DSS-AES256-GCM-SHA384:DHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA256:DHE-RSA-AES256-SHA:DHE-DSS-AES256-SHA:DHE-RSA-CAMELLIA256-SHA:DHE-DSS-CAMELLIA256-SHA:ECDH-RSA-AES256-GCM-SHA384:ECDH-ECDSA-AES256-GCM-SHA384:ECDH-RSA-AES256-SHA384:ECDH-ECDSA-AES256-SHA384:ECDH-RSA-AES256-SHA:ECDH-ECDSA-AES256-SHA:AES256-GCM-SHA384:AES256-SHA256:AES256-SHA:CAMELLIA256-SHA:PSK-AES256-CBC-SHA:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:SRP-DSS-AES-128-CBC-SHA:SRP-RSA-AES-128-CBC-SHA:SRP-AES-128-CBC-SHA:DHE-DSS-AES128-GCM-SHA256:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES128-SHA256:DHE-DSS-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA:DHE-RSA-SEED-SHA:DHE-DSS-SEED-SHA:DHE-RSA-CAMELLIA128-SHA:DHE-DSS-CAMELLIA128-SHA:ECDH-RSA-AES128-GCM-SHA256:ECDH-ECDSA-AES128-GCM-SHA256:ECDH-RSA-AES128-SHA256:ECDH-ECDSA-AES128-SHA256:ECDH-RSA-AES128-SHA:ECDH-ECDSA-AES128-SHA:AES128-GCM-SHA256:AES128-SHA256:AES128-SHA:SEED-SHA:CAMELLIA128-SHA:PSK-AES128-CBC-SHA:ECDHE-RSA-RC4-SHA:ECDHE-ECDSA-RC4-SHA:ECDH-RSA-RC4-SHA:ECDH-ECDSA-RC4-SHA:RC4-SHA:RC4-MD5:PSK-RC4-SHA:ECDHE-RSA-DES-CBC3-SHA:ECDHE-ECDSA-DES-CBC3-SHA:SRP-DSS-3DES-EDE-CBC-SHA:SRP-RSA-3DES-EDE-CBC-SHA:SRP-3DES-EDE-CBC-SHA:EDH-RSA-DES-CBC3-SHA:EDH-DSS-DES-CBC3-SHA:ECDH-RSA-DES-CBC3-SHA:ECDH-ECDSA-DES-CBC3-SHA:DES-CBC3-SHA:PSK-3DES-EDE-CBC-SHA:EDH-RSA-DES-CBC-SHA:EDH-DSS-DES-CBC-SHA:DES-CBC-SHA:EXP-EDH-RSA-DES-CBC-SHA:EXP-EDH-DSS-DES-CBC-SHA:EXP-DES-CBC-SHA:EXP-RC2-CBC-MD5:EXP-RC4-MD5";
+	const char		*ciphers = "DES-CBC3-SHA";
 	int			 rv = 0;
 
 	if ((buf = calloc(LEN, sizeof(char))) == NULL)
@@ -57,7 +61,12 @@ main()
 	TLS_INT(tls_config_set_ciphers(config, ciphers),
 	    "tls_config_set_ciphers");
 	tls_config_set_protocols(config, TLS_PROTOCOLS_ALL);
-        TLS_INT(tls_config_set_ca_file(config, "/etc/ssl/certs/ca-certificates.crt"), "tls_config_set_ca_file");
+
+	if (argc > 1)
+		TLS_INT(tls_config_set_ca_file(
+			    config,
+			    argv[1]),
+		    "tls_config_set_ca_file");
 
 	TLS_INT(tls_configure(ctx, config), "tls_configure");
 	TLS_INT(tls_connect(ctx, "mike-burns.com", "443"), "tls_connect");
